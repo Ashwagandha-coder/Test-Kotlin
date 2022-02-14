2323992817be33db0c533c105e8119f8a1923ea8
Ashwagandha-coder <max.cacz@yandex.ru>
2022-02-14 11:28:19 +0500

    one commit

---
+++ src/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+    var name:String = "Danil"
+
+    var a: Int = 5
+    var b: Int = 5
+        println("A + B = ${a + b}")
+        println("Hello $name")
+
+    }
