93cd4ff6d24a040b2b3cb50f8dec108f568bc2ce
Ashwagandha-coder <max.cacz@yandex.ru>
2022-02-18 11:13:34 +0500

    checkout

---
+++ src/Main2.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+    val ret = 56
+
+    val pop = "ruaq"
+
+    val ork = listOf("apple","banana","urka")
+
+    val tuq: MutableList<Int> = mutableListOf(1,34,78,99,22)
+
+    val qeq:ArrayList<Int> = arrayListOf(2,67,34,22,99)
+
+    val opa = setOf("aqop","ygv","yfhb")
+
+    val rua: MutableSet<Int> = mutableSetOf(2,4,67,44,222,111,999)
+}
