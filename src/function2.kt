f46b9eadc224688c5f3d8a63e79240541620ec65
Ashwagandha-coder <max.cacz@yandex.ru>
2022-02-18 12:20:59 +0500

    checkout 4

---
+++ src/function2.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+    println(popa(2,5))
+
+
+}
+
+fun popa(x: Int,y: Int) =  x + y
+
+fun ctroka(r: Int): String {
+
+    return "ngnfgn $r"
+}
+
+fun printEven(vararg num: Int) {
+
+
+}
