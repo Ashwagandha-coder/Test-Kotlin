cf4bb0a1aa30458efb4dafe7c4ad174118ac7cb8
Ashwagandha-coder <max.cacz@yandex.ru>
2022-02-18 11:52:48 +0500

    checkout 2

--- src/Main2.kt
+++ src/Main2.kt
@@ -13,4 +13,31 @@ fun main() {
     val opa = setOf("aqop","ygv","yfhb")
 
     val rua: MutableSet<Int> = mutableSetOf(2,4,67,44,222,111,999)
+
+    val b = rua.add(444)
+
+    var y = hashMapOf(4 to "apple",3 to "pear",2 to "peach")
+
+    val r: HashMap<Int, String> = hashMapOf(2 to "opa",1 to "ropa",9 to "jopa")
+
+    val rq = r.put(229,"yvd")
+
+    val pojn = sequenceOf(45,76,11,999,5555,32,76)
+
+    for (i in ork)
+        println(i)
+
+    var index = 0
+
+    while (index < ork.size) {
+        println(ork[index])
+        index ++
+
+    // Диапазон
+
+    println(5 in 3..10)
+
+    for (i in 1..10)
+        println(i)
+    }
 }
